c98cc5b0580c831cd455b3c90d66c6cea7eccda2
marten <kkx@protonmail.com>
2021-02-02 21:13:40 +0100

    added functions to parse the json format to a dataframe

--- fetch.py
+++ fetch.py
@@ -1,10 +1,18 @@
+#%%
 import tweepy 
 import pandas as pd
 import numpy as np
+import configparser
+import json
+#%%
+#authenticate with the twitter API using ini File
+config = configparser.ConfigParser()		
+config.read("config.ini")
 
-consumer_key = "zLhaEr6YbhAyUJDKykXt7rShh"
+keys = config["Twitter"]
 
-consumer_secret = "A8WoN8CERsebjyGAvTAVCDiDtixB5mQenvuszFxQ2A3YF4yc2Q"
+consumer_key = keys["con_key"]
+consumer_secret = keys["con_secret"]
 
 
 auth = tweepy.AppAuthHandler(consumer_key,consumer_secret)
@@ -13,54 +21,137 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 Cursor = tweepy.Cursor
 
 #%%
-df = pd.read_csv("partei/ids")
-
+df = pd.read_csv("data/ids.csv")
+df = df.drop(columns = "Unnamed: 0")
 
 #%%
 #get the last tweets of the mdbs
-df["tweets"] = None
-for i in range(len(df.name_id)):
-    u_id = df.name_id[i]
-    count = 50 
-    tweets = api.user_timeline(u_id, count = count, tweet_mode = "extended", exclude_replies = True)
-    df["tweets"][i] = tweets
+
+def tweet_getter(list_of_ids, count = 20 ):
+    """
+    get the last tweets for the desired list of ids,
+    additional argument = count, standard is 20 tweets
+     """
+    tweet_list = []
+
+    for i in range(len(list_if_ids)):
+        u_id = list_of_ids[i]
+        user_tweets = api.user_timeline(
+            u_id, count = count, tweet_mode = "extended", exclude_replies = False, include_rts = True
+            )
+
+        tweet_list.append(user_tweets)
+
+    return tweet_list
+        
+
+#%%
+
+id_list = df.name_id
+#caution: tweet_getter return has to be the same length as the tweets column
+df["tweets"] = tweet_getter(id_list)
 
 #this returns an incredibly large JSON, 
 #we want to extract the tweet text and possibly other metadata
 #%%
-# df["data"] = 0
-
-
-# for i in range(len(df.index)):
-#     dic = {}
-#     tweets= df.tweets[0]
-#     for t in tweets:
-#         t_id = t.id
-#         full_text = t.full_text
-#         date = t.created_at
-#         if "RT" in full_text:
-#             rt = True
-#         else:
-#             rt = False
-#         dic["t_id"] = 
-        
-        
-# try:
-#     for im in t.extended_entities["media"]:
-#         im_url = im["media_url_https"]
-# except:
-#     im_url = False
-            
-        
+#build the central dataframe for all tweet data
+tweet_df = pd.DataFrame(columns = 
+    ["user","t_text", "t_id", "t_date", "t_hashtags", "t_isrt", "t_isrpl", "other_users"]
+)
 #%%
-df["text"] = 0
-for i  in range(len(df.index)):
-    text = []
-    tweets = df["tweets"][i]
-    for t in tweets:
-        full_text = t.full_text
-        text.append(full_text)
-    df["text"][i] = text
+#build functions to extract tweet data
+def entitier(entity, typus):
+    """ helper function for tweet_check(), input = entity dict 
+    and a entity type, supported : "hashtags" and "users" """
+    if len(entity) == 0:
+        return None
     
-df.to_csv("texte")
-#this is huge (~500MB)
+    ents = []
+    for i in entity:
+
+        #there are different dictionarys depending on the selected entity
+        if typus == "hashtags":
+            extracted = i["text"]
+
+        elif typus == "users":
+            extracted = i["screen_name"]
+        else:
+            raise Exception("not a valid type of entity")
+
+        ents.append(extracted)
+
+    return ents
+
+
+def tweet_check(status):
+    """
+    this function extracts some helpful data about a tweet, 
+    input is a tweepy tweet object with related json 
+     """
+    t = status
+
+    #check if it is a retweet, call of status throws exception if it isnt
+    try:
+        rt = t.retweeted_status
+        t_isrt = True
+    except:
+        t_isrt = False
+
+    #if it is a retweet, it is often truncated,
+    # so we need to fetch the original tweet and extract data from there
+    if t_isrt:
+        t_text = t.retweeted_status.full_text
+        hashtags_dict = t.retweeted_status.entities["hashtags"]
+
+    else:
+        t_text = t.full_text
+        hashtags_dict = t.entities["hashtags"]
+
+
+
+    #checks if it is a reply, call to stats_id returns None if it isnt a reply
+    if type(t.in_reply_to_status_id) == type(None):
+        t_isrpl = False
+    else:
+        t_isrpl = True
+
+    #some general info about tweet
+    user = t.user.screen_name
+    t_id = t.id
+    t_date = t.created_at
+    other_u_dict = t.entities["user_mentions"]
+
+    #the hashtags and other users return a list of dictionarys, which coulb be len 0,
+    #so we define another function to check and extract the data
+    other_users = entitier(other_u_dict, "users")
+    t_hashtags = entitier(hashtags_dict, "hashtags")
+
+    
+    return {
+        "user": user, "t_text": t_text, "t_id": t_id, 
+        "t_date": t_date, "t_hashtags": t_hashtags, 
+        "t_isrt": t_isrt, "t_isrpl": t_isrpl, "other_users": other_users
+    }
+
+
+def tweet_parser(list_of_status_objs):
+    """ 
+    extracts the data for all the tweet objects in the list,
+    returns a central  dataframe 
+    """
+    tweet_df = pd.DataFrame(columns = 
+        ["user","t_text", "t_id", "t_date", "t_hashtags",
+         "t_isrt", "t_isrpl", "other_users"]
+    )
+    for status in list_of_status_objs:
+        dic = tweet_check(status)
+        tweet_df = tweet_df.append(dic, ignore_index=True)
+
+    return tweet_df
+
+
+
+#%%
+
+# %%
+
